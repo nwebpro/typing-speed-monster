cf2d1f19ff24e0704ec0d0f99b50ba317f86ce15
nwebpro <nwebpro.work@gmail.com>
2022-09-15 00:49:11 +0600

    quetion title change

--- history.js
+++ history.js
@@ -31,7 +31,7 @@ function displayHistory() {
 
     newRow.innerHTML = `
   <h3 class="bold">${test.questionText}</h3>
-  <p>You took: <span class="bold">${parseInt(test.timeTaken)}</span> seconds</p>
+  <p>You took: <span class="bold">${Math.round(test.timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
